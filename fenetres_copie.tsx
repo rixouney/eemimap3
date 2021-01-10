d65e9e4796c36145496f40f8c9969d64c5ebf1f7
rixouney <tinebinal.reda@gmail.com>
2021-01-10 20:53:33 +0100

    new elements added to the map

---
+++ fenetres_copie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fenetres_copie" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="fenetres_copie.png" width="106" height="118"/>
+</tileset>
---
+++ fenetres_ext_int_original.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fenetres_ext_int_original" tilewidth="32" tileheight="32" tilecount="182" columns="13">
+ <image source="fenetres_ext_int_original.png" width="424" height="469"/>
+</tileset>
