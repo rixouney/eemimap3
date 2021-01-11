5cf388ff2990e1198c52f70d0f4d81799f80765b
rixouney <tinebinal.reda@gmail.com>
2021-01-11 19:27:42 +0100

    fix jitsi room and first version of palais brogniart

---
+++ palais_brongniart_exterieur - 64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="palais_brongniart_exterieur - 64" tilewidth="64" tileheight="64" tilecount="100" columns="10">
+ <image source="palais_brongniart_exterieur - 64.png" width="640" height="640"/>
+</tileset>
---
+++ palais_brongniart_exterieur.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="palais_brongniart_exterieur" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="palais_brongniart_exterieur.png" width="640" height="640"/>
+</tileset>
