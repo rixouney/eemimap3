3ee953b567536a1f3f38b38063b9de922a16540b
rixouney <tinebinal.reda@gmail.com>
2021-01-11 20:08:21 +0100

    new map by Caroline

---
+++ fenetres_ext_int.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fenetres_ext_int" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="fenetres_ext_int.png" width="141" height="157"/>
+</tileset>
